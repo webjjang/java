99371ee41ae30dcb5f5fbb168baa2de8de5f4f74
webjjang <leelj1@nate.com>
2026-01-21 14:09:22 +0900

    Add files via upload

--- 202512/project/src/com/webjjang/member/dao/MemberDAO.java
+++ 202512/project/src/com/webjjang/member/dao/MemberDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.webjjang.member.vo.LoginVO;
 import com.webjjang.member.vo.MemberVO;
@@ -26,7 +28,7 @@ public class MemberDAO {
 	final String UPW = "java";
 	
 
-	// 로그인 처리 - 데이터 가져오기(R) - 1개 데이터 가져오기.(view)
+	// 1-1. 로그인 처리 - 데이터 가져오기(R) - 1개 데이터 가져오기.(view)
 	public LoginVO login(LoginVO userVO) throws Exception{
 		LoginVO vo = null;
 		
@@ -59,7 +61,9 @@ public class MemberDAO {
 		return vo;
 	} // login()의 끝
 	
-	// 3. 회원가입
+	// 1-2. 로그아웃 처리 - DB 사용하지 않음.
+	
+	// 2-1. 회원가입
 	public Integer write(MemberVO vo) throws Exception {
 		Integer result = 0;
 		
@@ -89,7 +93,7 @@ public class MemberDAO {
 		return result;
 	} // write()의 끝
 
-	// 아이디 찾기 처리 - 데이터 가져오기(R) - 1개 데이터 가져오기.(view)
+	// 3-1 . 아이디 찾기 처리 - 데이터 가져오기(R) - 1개 데이터 가져오기.(view)
 	public String searchId(MemberVO vo) throws Exception{
 		String id = null;
 		
@@ -117,7 +121,7 @@ public class MemberDAO {
 		return id;
 	} // seachId()의 끝
 	
-	// 비밀번호 찾기 정보 확인 - 데이터 가져오기(R) - 1개 데이터 가져오기.(view)
+	// 4-1. 비밀번호 찾기 정보 확인 - 데이터 가져오기(R) - 1개 데이터 가져오기.(view)
 	public String checkPw(MemberVO vo) throws Exception{
 		String id = null;
 		
@@ -146,7 +150,7 @@ public class MemberDAO {
 		return id;
 	} // checkPw()의 끝
 	
-	// 비밀번호 변경
+	// 3-2. 비밀번호 변경
 	// user = 1 로그인한 사용자가 변경하고 있다. 현재 비밀번호를 붙인다.
 	// user = 0 비밀번호 찾기에서 임시비밀번호를 저장해야 하므로 현재 비밀번호가 없다.
 	public Integer changePw(MemberVO vo, Integer user) throws Exception {
@@ -174,5 +178,55 @@ public class MemberDAO {
 		return result;
 	} // changePw()의 끝
 
+	// ---------------- 관리자 메뉴 ----------------
+	// 5. 회원 리스트
+	public List<MemberVO> list() throws Exception {
+		List<MemberVO> list = null;
+		
+		list = new ArrayList<>();
+		
+		// 1. 드라이버 확인 - static으로 선언된 내용이 자동으로 올라간다.
+		Class.forName(DRIVER);
+		
+		// 2. 연결 객체 - 정보를 넣고 서버에 다녀온다.
+		// getConnection() - static
+		con = DriverManager.getConnection(URL, UID, UPW);
+		
+		// 3. 실행할 쿼리 작성
+		String sql = "select m.id, m.name, m.gender, to_char(m.birth, 'yyyy-mm-dd') birth, "
+				+ " m.tel, m.email, m.gradeNo, g.gradeName "
+				+ " from member m, grade g where (m.gradeNo = g.gradeNo) order by id";
+		
+		// 4. 실행 객체 & 데이터 세팅
+		pstmt = con.prepareStatement(sql);
+		
+		// 5. 실행 : select :executeQuery() -> rs, insert / update / delete :executeUpdate() -> Integer
+		rs = pstmt.executeQuery();
+		
+		// 6. DB에서 가져온 데이터 채우기
+		if(rs != null) {
+			while(rs.next()) { // 데이터가 있는 만큼 반복 실행
+				// 저장할 객체를 생성한다.
+				MemberVO vo = new MemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setName(rs.getString("name"));
+				vo.setGender(rs.getString("gender"));
+				vo.setBirth(rs.getString("birth"));
+				vo.setTel(rs.getString("tel"));
+				vo.setEmail(rs.getString("email"));
+				vo.setGradeNo(rs.getInt("gradeNo"));
+				vo.setGradeName(rs.getString("gradeName"));
+				
+				list.add(vo);
+			}// while의 끝
+		}// if의 끝
+
+		// 7. DB 닫기
+		con.close();
+		pstmt.close();
+		rs.close();
+		
+		return list;
+	} // list()의 끝
 
 } // 클래스의 끝
