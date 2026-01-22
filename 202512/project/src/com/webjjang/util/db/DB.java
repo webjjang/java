18b36299ceee563bd29a989d444ece12ff4f1589
webjjang <leelj1@nate.com>
2026-01-22 14:35:04 +0900

    DB 클래스 작성

---
+++ 202512/project/src/com/webjjang/util/db/DB.java
@@ -0,0 +1,44 @@
+package com.webjjang.util.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DB {
+
+	// DB 연결 정보
+	private static final String DRIVER = "oracle.jdbc.OracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String UID = "java";
+	private static final String UPW = "java";
+
+	static {
+		// 1. 드라이버 확인
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// DB.getConnection()
+	public static Connection getConnection() throws Exception{
+		// 2. 연결 객체
+		return DriverManager.getConnection(URL, UID, UPW);
+	}
+	
+	// 사용했던 객체 닫기 - insert, update, delete
+	public static void close(Connection con, PreparedStatement pstmt) throws Exception {
+		if(con != null) con.close();
+		if(pstmt != null) pstmt.close();
+	}
+	
+	// 사용했던 객체 닫기 - insert, update, delete
+	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) throws Exception {
+		close(con, pstmt);
+		if(rs != null) rs.close();
+	}
+	
+}
